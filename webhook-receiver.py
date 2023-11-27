25d7ad3b2ec1541e9ef1e5578e562a6b4e4dba5d
emonite <118502417+emonite@users.noreply.github.com>
2023-11-26 23:03:29 -0500

    Update webhook-receiver.py

--- webhook-receiver.py
+++ webhook-receiver.py
@@ -1,17 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Get data from the webhook
-    data = request.get_data(as_text=True)
-    # You can now process the data as plain text
-    print("Received data:", data)
+    # Get JSON data from the webhook
+    data = request.json
 
+    # Print the received JSON data
+    print("Received JSON data:", data)
+
+    # You can now process the data
+    # For example, you can access values like data['key_name']
 
     # Return a response
-    return 'Success', 200
+    return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
