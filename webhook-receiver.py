d5d3d1df7a4594ddf28d40a81ed6d8e6fbc0baaf
emonite <118502417+emonite@users.noreply.github.com>
2023-11-26 22:27:32 -0500

    Update webhook-receiver.py

--- webhook-receiver.py
+++ webhook-receiver.py
@@ -1,8 +1,5 @@
 from flask import Flask, request
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
@@ -17,4 +14,4 @@ def webhook():
     return 'Success', 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
